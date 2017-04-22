7167f183b7b9fbe0283ab306118cc5b3291f9086
BinnyK <BinnyK@users.noreply.github.com>
2017-04-23 00:00:59 +1000

    Basic layout for show page

---
+++ twitter_response.rb
@@ -0,0 +1,197 @@
+--- !ruby/object:Twitter::Tweet
+attrs:
+  :created_at: Sat Apr 22 13:25:00 +0000 2017
+  :id: 855774440969441280
+  :id_str: '855774440969441280'
+  :text: 'Good at #Coding here are four #Careers You will Love https://t.co/AbRPzhP1HC
+    #Careeradvice'
+  :truncated: false
+  :entities: &2
+    :hashtags:
+    - :text: Coding
+      :indices:
+      - 8
+      - 15
+    - :text: Careers
+      :indices:
+      - 30
+      - 38
+    - :text: Careeradvice
+      :indices:
+      - 77
+      - 90
+    :symbols: []
+    :user_mentions: []
+    :urls:
+    - :url: https://t.co/AbRPzhP1HC
+      :expanded_url: https://youforma.com/article/Good-at-Coding-here-are-four-Careers-You-will-Love/58738e988905021b121f5089
+      :display_url: youforma.com/article/Good-a…
+      :indices:
+      - 53
+      - 76
+  :metadata: &3
+    :iso_language_code: en
+    :result_type: recent
+  :source: <a href="https://about.twitter.com/products/tweetdeck" rel="nofollow">TweetDeck</a>
+  :in_reply_to_status_id: 
+  :in_reply_to_status_id_str: 
+  :in_reply_to_user_id: 
+  :in_reply_to_user_id_str: 
+  :in_reply_to_screen_name: 
+  :user:
+    :id: 212841660
+    :id_str: '212841660'
+    :name: socialmedia
+    :screen_name: socialmediasmq
+    :location: India
+    :description: "#SocialMedia monitoring, #marketing & #brand management team of
+      @AnanthV9 @YourSMQ @Techdivine Creative Services #DigitalAgency #DigitalMarketing
+      #Mumbai"
+    :url: http://t.co/zfRR2wABSp
+    :entities: &1
+      :url:
+        :urls:
+        - :url: http://t.co/zfRR2wABSp
+          :expanded_url: http://www.techdivine.com
+          :display_url: techdivine.com
+          :indices:
+          - 0
+          - 22
+      :description:
+        :urls: []
+    :protected: false
+    :followers_count: 1977
+    :friends_count: 1089
+    :listed_count: 1143
+    :created_at: Sun Nov 07 06:04:35 +0000 2010
+    :favourites_count: 608
+    :utc_offset: 
+    :time_zone: 
+    :geo_enabled: false
+    :verified: false
+    :statuses_count: 11867
+    :lang: en
+    :contributors_enabled: false
+    :is_translator: false
+    :is_translation_enabled: false
+    :profile_background_color: C0DEED
+    :profile_background_image_url: http://abs.twimg.com/images/themes/theme1/bg.png
+    :profile_background_image_url_https: https://abs.twimg.com/images/themes/theme1/bg.png
+    :profile_background_tile: false
+    :profile_image_url: http://pbs.twimg.com/profile_images/1368021128/tw_8595923_1306297318_normal.jpg
+    :profile_image_url_https: https://pbs.twimg.com/profile_images/1368021128/tw_8595923_1306297318_normal.jpg
+    :profile_banner_url: https://pbs.twimg.com/profile_banners/212841660/1471845335
+    :profile_link_color: 1DA1F2
+    :profile_sidebar_border_color: C0DEED
+    :profile_sidebar_fill_color: DDEEF6
+    :profile_text_color: '333333'
+    :profile_use_background_image: true
+    :has_extended_profile: false
+    :default_profile: true
+    :default_profile_image: false
+    :following: 
+    :follow_request_sent: 
+    :notifications: 
+    :translator_type: none
+  :geo: 
+  :coordinates: 
+  :place: 
+  :contributors: 
+  :is_quote_status: false
+  :retweet_count: 0
+  :favorite_count: 0
+  :favorited: false
+  :retweeted: false
+  :possibly_sensitive: false
+  :lang: en
+_memoized_method_cache: !ruby/object:Memoizable::Memory
+  memory: !ruby/object:ThreadSafe::Cache
+    backend:
+      :user: !ruby/object:Twitter::User
+        attrs:
+          :id: 212841660
+          :id_str: '212841660'
+          :name: socialmedia
+          :screen_name: socialmediasmq
+          :location: India
+          :description: "#SocialMedia monitoring, #marketing & #brand management team
+            of @AnanthV9 @YourSMQ @Techdivine Creative Services #DigitalAgency #DigitalMarketing
+            #Mumbai"
+          :url: http://t.co/zfRR2wABSp
+          :entities: *1
+          :protected: false
+          :followers_count: 1977
+          :friends_count: 1089
+          :listed_count: 1143
+          :created_at: Sun Nov 07 06:04:35 +0000 2010
+          :favourites_count: 608
+          :utc_offset: 
+          :time_zone: 
+          :geo_enabled: false
+          :verified: false
+          :statuses_count: 11867
+          :lang: en
+          :contributors_enabled: false
+          :is_translator: false
+          :is_translation_enabled: false
+          :profile_background_color: C0DEED
+          :profile_background_image_url: http://abs.twimg.com/images/themes/theme1/bg.png
+          :profile_background_image_url_https: https://abs.twimg.com/images/themes/theme1/bg.png
+          :profile_background_tile: false
+          :profile_image_url: http://pbs.twimg.com/profile_images/1368021128/tw_8595923_1306297318_normal.jpg
+          :profile_image_url_https: https://pbs.twimg.com/profile_images/1368021128/tw_8595923_1306297318_normal.jpg
+          :profile_banner_url: https://pbs.twimg.com/profile_banners/212841660/1471845335
+          :profile_link_color: 1DA1F2
+          :profile_sidebar_border_color: C0DEED
+          :profile_sidebar_fill_color: DDEEF6
+          :profile_text_color: '333333'
+          :profile_use_background_image: true
+          :has_extended_profile: false
+          :default_profile: true
+          :default_profile_image: false
+          :following: 
+          :follow_request_sent: 
+          :notifications: 
+          :translator_type: none
+          :status:
+            :created_at: Sat Apr 22 13:25:00 +0000 2017
+            :id: 855774440969441280
+            :id_str: '855774440969441280'
+            :text: 'Good at #Coding here are four #Careers You will Love https://t.co/AbRPzhP1HC
+              #Careeradvice'
+            :truncated: false
+            :entities: *2
+            :metadata: *3
+            :source: <a href="https://about.twitter.com/products/tweetdeck" rel="nofollow">TweetDeck</a>
+            :in_reply_to_status_id: 
+            :in_reply_to_status_id_str: 
+            :in_reply_to_user_id: 
+            :in_reply_to_user_id_str: 
+            :in_reply_to_screen_name: 
+            :geo: 
+            :coordinates: 1
+            :place: 
+            :contributors: 
+            :is_quote_status: false
+            :retweet_count: 0
+            :favorite_count: 0
+            :favorited: false
+            :retweeted: false
+            :possibly_sensitive: false
+            :lang: en
+        _memoized_method_cache: !ruby/object:Memoizable::Memory
+          memory: !ruby/object:ThreadSafe::Cache
+            backend:
+              :screen_name: socialmediasmq
+            default_proc: 
+          monitor: !ruby/object:Monitor
+            mon_owner: 
+            mon_count: 0
+            mon_mutex: !ruby/object:Thread::Mutex {}
+      :text: 'Good at #Coding here are four #Careers You will Love https://t.co/AbRPzhP1HC
+        #Careeradvice'
+    default_proc: 
+  monitor: !ruby/object:Monitor
+    mon_owner: 
+    mon_count: 0
+    mon_mutex: !ruby/object:Thread::Mutex {}
