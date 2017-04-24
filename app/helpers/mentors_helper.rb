6fbc3466254f7162af50c8fca6025c98ede5931d
BinnyK <BinnyK@users.noreply.github.com>
2017-04-24 16:50:18 +1000

    Tidy style for mentor index page

--- app/helpers/mentors_helper.rb
+++ app/helpers/mentors_helper.rb
@@ -1,2 +1,10 @@
 module MentorsHelper
+
+	def set_bg(model)
+		"background-image:url(#{model.profile_banner_url});
+		background-position: center center;
+		background-repeat: no-repeat;
+		background-size: cover;"
+	end
+
 end
