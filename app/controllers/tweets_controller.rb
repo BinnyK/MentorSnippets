70c54a0f323d6d4d8705d5881dd382c48581ef9a
BinnyK <BinnyK@users.noreply.github.com>
2017-04-25 15:14:19 +1000

    Add twitter keys to env variable

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,8 +6,8 @@ class TweetsController < ApplicationController
 
   def twitter_client
     @twitter_client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key        = "QmnF0N1nALWJCshk5ecUWXG2n"
-      config.consumer_secret     = "N7PoX41YI7YHinZw5qI9XmOskX0nzw60GPtMuQGb7UY6oNh853"
+      config.consumer_key        = ENV["TW_ID"]
+      config.consumer_secret     = ENV["TW_SECRET"]
     end
   end
 
